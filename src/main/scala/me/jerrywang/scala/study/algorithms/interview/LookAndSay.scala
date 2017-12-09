38db66e25e06dcaac2f5554645e8e5d6e4dfb21b
Jerry Wang <j3rry.wan9@gmail.com>
2017-12-08 17:51:16 -0800

    update me.jerrywang.scala.study.algorithms.interview.LookAndSay.scala

--- src/main/scala/me/jerrywang/scala/study/algorithms/interview/LookAndSay.scala
+++ src/main/scala/me/jerrywang/scala/study/algorithms/interview/LookAndSay.scala
@@ -15,33 +15,33 @@ package me.jerrywang.scala.study.algorithms.interview
   * encode(1, 4) = "111221"
   */
 object LookAndSay extends App {
-  def encode(seed: Int, iterations: Int): String = {
-    var input = seed.toString
+  def encode(seed: Int, iterations: Int): String = iterations match {
+    case i if i < 1 => seed.toString
+    case _ => {
+      var input = seed.toString
+      var retVal = ""
+      var prev = input.substring(0, 1)
+      var current = input.substring(0, 1)
 
-    if (iterations < 1) return input
-
-    var retVal = ""
-    var prev = input.substring(0, 1)
-    var current = input.substring(0, 1)
-
-    for (_ <- 0 until iterations) {
-      var counter = 0
-      for (j <- 0 until input.length) {
-        current = input.substring(j, j + 1)
-        if (current.equals(prev)) counter += 1
-        else {
-          retVal = retVal + counter.toString + prev
-          counter = 1
-          prev = current
+      for (_ <- 0 until iterations) {
+        var counter = 0
+        for (j <- 0 until input.length) {
+          current = input.substring(j, j + 1)
+          if (current.equals(prev)) counter += 1
+          else {
+            retVal = retVal + counter.toString + prev
+            counter = 1
+            prev = current
+          }
         }
+        retVal = retVal + counter.toString + prev
+        input = retVal
+        prev = input.substring(0, 1)
+        current = input.substring(0, 1)
+        retVal = ""
       }
-      retVal = retVal + counter.toString + prev
-      input = retVal
-      prev = input.substring(0, 1)
-      current = input.substring(0, 1)
-      retVal = ""
+      input
     }
-    return input
   }
 
   println(encode(1, 4))
