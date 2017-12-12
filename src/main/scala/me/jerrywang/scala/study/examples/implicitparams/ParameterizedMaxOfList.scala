24af1a5f22cad5ff60b9d47fe49d9b03f3a30a54
Jerry Wang <j3rry.wan9@gmail.com>
2017-12-11 16:08:24 -0800

    add me.jerrywang.scala.study.examples.implicitparams.ParameterizedMaxOfList.scala

---
+++ src/main/scala/me/jerrywang/scala/study/examples/implicitparams/ParameterizedMaxOfList.scala
@@ -0,0 +1,18 @@
+package me.jerrywang.scala.study.examples.implicitparams
+
+/**
+  * A demo of context bound.
+  */
+object ParameterizedMaxOfList extends App {
+  def maxOfList[T: Ordering](elements: List[T]): T = elements match {
+    case List() => throw new IllegalArgumentException("Empty list!")
+    case List(x) => x
+    case x :: rest =>
+      val maxOfRest = maxOfList(rest)  //
+      if (implicitly[Ordering[T]].gt(x, maxOfRest)) x
+      else maxOfRest
+  }
+
+  println(maxOfList(List(2, -4, 5, 7, 1)))
+  println(maxOfList(List("apple", "pineapple", "orange", "banana")))
+}
