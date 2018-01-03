b712a6294847d0ad2c7c0fa49326355d5b831f78
Jerry Wang <j3rry.wan9@gmail.com>
2018-01-03 13:13:12 -0800

    add an example for using ScalaCheck with ScalaTest

---
+++ src/test/scala/me/jerrywang/scala/study/algorithms/LookAndSaySpec.scala
@@ -0,0 +1,20 @@
+package me.jerrywang.scala.study.algorithms
+
+import org.scalacheck.Gen
+import org.scalatest.Matchers._
+import org.scalatest.PropSpec
+import org.scalatest.prop.PropertyChecks
+
+import me.jerrywang.scala.study.algorithms.interview.LookAndSay
+
+class LookAndSaySpec extends PropSpec with PropertyChecks {
+  val randomSeed = Gen.choose(0, Int.MaxValue)
+  val iterationLessThanOne = Gen.choose(-Int.MaxValue, 0)
+
+  property("the encode method must handle less than 1 iterations") {
+    forAll(randomSeed, iterationLessThanOne) { (seed, iteration) =>
+      val output = LookAndSay.encode(seed, iteration)
+      output should be (seed.toString)
+    }
+  }
+}
