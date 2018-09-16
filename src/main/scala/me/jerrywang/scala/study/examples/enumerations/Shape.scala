d3bd8bc52b0a858e1e22f8140ecf92182ed6639b
Jerry Wang <j3rry.wan9@gmail.com>
2018-09-15 23:41:54 -0700

    add an implicit conversion from an enumerated value to string

--- src/main/scala/me/jerrywang/scala/study/examples/enumerations/Shape.scala
+++ src/main/scala/me/jerrywang/scala/study/examples/enumerations/Shape.scala
@@ -2,12 +2,29 @@ package me.jerrywang.scala.study.examples.enumerations
 
 import me.jerrywang.scala.study.examples.enumerations.Color._
 
-case class Shape(name: String, color: Color) {
+/**
+  * A shape with color.
+  *
+  * @param name the name of the shape.
+  * @param color the color of the shape.
+  */
+case class Shape(name: String, color: String) {
   override def toString = s"A $name in $color"
 }
 
 object Shape {
-  def main(args: Array[String]) = {
+  /** Enables implicit conversions. */
+  import scala.language.implicitConversions
+
+  /**
+    * Converts an enumerated value to string.
+    *
+    * @param value the enumerated value to be converted.
+    * @tparam A the type extends `scala.Enumeration`.
+    */
+  implicit def enumeratedValueToString[A <: Enumeration](value: A#Value): String = value.toString
+
+  def main(args: Array[String]): Unit = {
     val redCircle = Shape("circle", Red)
     println(redCircle)
   }
