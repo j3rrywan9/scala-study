7968bce513b9ccc33c23719fe9033531d82660e0
Jerry Wang <j3rry.wan9@gmail.com>
2018-10-22 16:24:30 -0700

    add an example for scala-logging

--- src/main/scala/me/jerrywang/scala/study/algorithms/interview/Scrabble.scala
+++ src/main/scala/me/jerrywang/scala/study/algorithms/interview/Scrabble.scala
@@ -2,11 +2,13 @@ package me.jerrywang.scala.study.algorithms.interview
 
 import java.io.{ FileNotFoundException, IOException }
 
+import com.typesafe.scalalogging.LazyLogging
+
 import scala.collection.mutable.{ ListBuffer, Map }
 import scala.io.Source
 import scala.io.StdIn.readLine
 
-object Scrabble extends App {
+object Scrabble extends App with LazyLogging {
   val WordsPath = "/usr/share/dict/words"
   val map = Map.empty[String, ListBuffer[String]]
 
@@ -20,7 +22,7 @@ object Scrabble extends App {
       }
     }
 
-    // map.foreach { case (sortedWord, listOfAnagrams) => println(sortedWord + " -> " + listOfAnagrams.mkString("(", ", ", ")")) }
+    map.foreach { case (sortedWord, listOfAnagrams) => logger.debug(sortedWord + " -> " + listOfAnagrams.mkString("(", ", ", ")")) }
 
     while (true) {
       val userInput = readLine("What letters? ")
