759ae38212cffc27af99d5348a1a03b41f580d07
Jerry Wang <j3rry.wan9@gmail.com>
2019-03-08 23:17:30 -0800

    delete src/main/scala/upper2.scala

--- src/main/scala/upper2.scala
+++
@@ -1,9 +0,0 @@
-object Upper {
-  def upper(strings: String*) = strings.map(_.toUpperCase())
-}
-
-object upper2 {
-  def main(args: Array[String]): Unit = {
-    println(Upper.upper("A", "First", "Scala", "Program"))
-  }
-}
