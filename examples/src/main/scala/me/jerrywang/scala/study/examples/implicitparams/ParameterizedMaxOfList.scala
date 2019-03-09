1a87b03622d628e3983646bb133751f207336c0a
Jerry Wang <j3rry.wan9@gmail.com>
2019-03-08 23:42:28 -0800

    add examples module

--- examples/src/main/scala/me/jerrywang/scala/study/examples/implicitparams/ParameterizedMaxOfList.scala
+++ examples/src/main/scala/me/jerrywang/scala/study/examples/implicitparams/ParameterizedMaxOfList.scala
@@ -1,14 +1,14 @@
 package me.jerrywang.scala.study.examples.implicitparams
 
 /**
-  * A demo of context bound.
-  */
+ * A demo of context bound.
+ */
 object ParameterizedMaxOfList extends App {
   def maxOfList[T: Ordering](elements: List[T]): T = elements match {
-    case List() => throw new IllegalArgumentException("Empty list!")
+    case List()  => throw new IllegalArgumentException("Empty list!")
     case List(x) => x
     case x :: rest =>
-      val maxOfRest = maxOfList(rest)  //
+      val maxOfRest = maxOfList(rest) // (ordering) is implicit
       if (implicitly[Ordering[T]].gt(x, maxOfRest)) x
       else maxOfRest
   }
--- examples/src/main/scala/me/jerrywang/scala/study/examples/typesafeconfig/AppConfig.scala
+++ examples/src/main/scala/me/jerrywang/scala/study/examples/typesafeconfig/AppConfig.scala
@@ -1,14 +1,22 @@
 package me.jerrywang.scala.study.examples.typesafeconfig
 
+import java.nio.file.{ Path, Paths }
+
 import com.typesafe.config.{ Config, ConfigFactory }
 
+import scala.util.Try
+
 object AppConfig {
-  val Conf: Config = ConfigFactory.load()
+
+  private val Conf: Config = ConfigFactory.load()
 
   val ProcessTimeout: Int = Conf.getInt("process.timeout.minutes")
+
+  val uuids = Try(Paths.get(Conf.getString("uuids.path"))).toOption
 
   object S3Config {
     val RegionName: String = Conf.getString("aws.region")
     val OutputBucket: String = Conf.getString("output.bucket")
   }
+
 }
