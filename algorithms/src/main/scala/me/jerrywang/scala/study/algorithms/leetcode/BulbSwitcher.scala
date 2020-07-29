57520b86beaf93a385c14a26e103353f530e5556
Jerry Wang <j3rry.wan9@gmail.com>
2020-07-29 00:20:58 -0700

    add Bulb Switcher

---
+++ algorithms/src/main/scala/me/jerrywang/scala/study/algorithms/leetcode/BulbSwitcher.scala
@@ -0,0 +1,11 @@
+package me.jerrywang.scala.study.algorithms.leetcode
+
+import scala.math
+
+// LC 319
+object BulbSwitcher {
+
+  def bulbSwitch(n: Int): Int = {
+    math.sqrt(n).toInt
+  }
+}
---
+++ algorithms/src/test/scala/me/jerrywang/scala/study/algorithms/leetcode/BulbSwitcherTest.scala
@@ -0,0 +1,10 @@
+package me.jerrywang.scala.study.algorithms.leetcode
+
+import org.scalatest.FunSuite
+
+class BulbSwitcherTest extends FunSuite {
+
+  test("testExample1") {
+    assert(BulbSwitcher.bulbSwitch(3) == 1)
+  }
+}
